95ab85532d8bcbb3d54586773f5ba3e0437d5cfb
Sun <ezample@gmail.com>
2022-04-30 22:02:33 +0200

    almost done

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-const clickListener = document.querySelector('.link_left');
-const sliderOne = document.querySelector('.slider__one');
-const sliderTwo = document.querySelector('.slider__two');
-const sliderThree = document.querySelector('.slider__three');
-const sliderLine = document.querySelector('.slider__items');
+const slides = document.querySelectorAll('.slider__item');
+const prev = document.querySelector('.link_left');
+const next = document.querySelector('.link_right');
+const dots = document.querySelectorAll('.dots');
 
-let offset = 0;
+let slideIndex = 2;
+let dotsIndex = 2;
 
-clickListener.addEventListener('click', () => {
-    offset += 550;
-    sliderTwo.classList.remove('slider__two')
-    sliderTwo.classList.add('slider__three')
-    sliderTwo.style.left = offset;
-    console.log(1)
-});
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+    showDots(dotsIndex += 1);
+}
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+    showDots(dotsIndex -=1);
+}
+
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (let slide of slides) {
+        slide.classList.add('slider__small');
+        slide.classList.remove('slider__big');
+    }   
+    slides[slideIndex - 1].classList.add('slider__big'); 
+    }
+
+function showDots(n) {
+    if (n > dots.length) {
+        dotsIndex = 1
+    }
+    if (n < 1) {
+        dotsIndex = dots.length;
+    }
+    for (let d of dots) {
+        d.classList.remove('active');
+    }   
+    dots[dotsIndex - 1].classList.add('active'); 
+}
